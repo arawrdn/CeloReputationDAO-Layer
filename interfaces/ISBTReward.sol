5e316f6b7bf7504772f77cea7bd963a61c1aa348
0xward <aradeawardana97@gmail.com>
2025-12-11 07:48:44 +0700

    Create ISBTReward.sol

---
+++ interfaces/ISBTReward.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ISBTReward {
+    function hasSBT(address _user, uint256 _sbtTierId) external view returns (bool);
+    function getHighestSBTLevel(address _user) external view returns (uint256);
+}
